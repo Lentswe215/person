039b7d19d0953d696dddd890137340d3467720f2
Lentswe215 <ephraimmamonong@gmail.com>
2020-01-21 11:49:17 +0200

    fixed spec

--- spec/person_spec.js
+++ spec/person_spec.js
@@ -3,15 +3,15 @@ let {Person} = require('../src/person')
 let personTest = new Person('Ryan',30,'male',['being a hardarse','agile', 'ssd hard drives']) 
 
 describe("personTest", ()=>{
-    it("should return the with Ryan", ()=>{
+    it("should return Ryan", ()=>{
 
         expect(personTest.name).toBe("Ryan")
     })
-    it("should return the with 30", ()=>{
+    it("should return 30", ()=>{
 
         expect(personTest.age).toBe(30)
     })   
-    it("should return the with 'Ryan',30,'male',['being a hardarse','agile', 'ssd hard drives']", ()=>{
+    it("should return Hello, my name is Ryan and I am 30 years old. My interests are being a hardarse, agile and ssd hard drives.", ()=>{
 
         expect(personTest.hello()).toBe("Hello, my name is Ryan and I am 30 years old. My interests are being a hardarse, agile and ssd hard drives.")
     }) 
